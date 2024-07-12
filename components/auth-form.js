dddbbbbe874e530a498aa69663ea91d77c90e7d1
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2024-07-12 19:41:01 +0300

    Feat(components\auth-form.js): Create switch mode for login and signup switching

--- components/auth-form.js
+++ components/auth-form.js
@@ -4,7 +4,8 @@ import Link from "next/link";
 import { useFormState } from "react-dom";
 import { signup } from "@/actions/auth-actions";
 
-export default function AuthForm() {
+export default function AuthForm({ mode }) {
+  // 'login', 'signup'
   const [formState, formAction] = useFormState(signup, {});
 
   return (
@@ -28,10 +29,17 @@ export default function AuthForm() {
         </ul>
       )}
       <p>
-        <button type="submit">Create Account</button>
+        <button type="submit">
+          {mode === "login" ? "Login" : "Create Account"}
+        </button>
       </p>
       <p>
-        <Link href="/">Login with existing account.</Link>
+        {mode === "login" && (
+          <Link href="/?mode=signup">Create an Account.</Link>
+        )}
+        {mode === "signup" && (
+          <Link href="/?mode=login">Login with existing account.</Link>
+        )}
       </p>
     </form>
   );
